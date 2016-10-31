27d382e67c4aace4b0095d3978408635bcedf77a
Gabriel De La O <gabriel@cohesionlabs.io>
2016-10-31 18:08:35 -0500

    Member Berry.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 var fs = require('fs');
-
 /**
  * Adds commas to a number
  * @param {number} number
@@ -29,9 +28,15 @@ module.exports = (function(){
 		createFile.then( function(res) {
 			// Create the files.
 			componentsList.map( function(component){
-				fs.writeFile(dir+"/"+component+".jsx", "import React, { Component } from 'react';", function(err) {
-					console.log('error', err);
-				});	
+				if( typeof component === "object" ) {
+					fs.writeFile(dir+"/"+component.name+".jsx", jsxTemplate(component.name, component.type) , function(err) {
+						console.log('error', err);
+					});
+				} else {
+					fs.writeFile(dir+"/"+component+".jsx", jsxTemplate(component, 'defualt') , function(err) {
+						console.log('error', err);
+					});	
+				}	
 			});
 			
 			console.log("Member? Ohhh I member!");
@@ -39,5 +44,51 @@ module.exports = (function(){
 
 	}
 
+	function jsxTemplate(name, type) {
+		//var type = 'default';
+		console.log('what came in name', name);
+		console.log('what came in type', type);
+		let contents = ``;
+
+		switch(type) {
+			case "pure":
+				contents = pure.call(contents, name);
+				break;
+			case "other":
+				break;
+			default:
+				contents = es6.call(contents, name);
+		} // End case.
+
+		return contents;
+	}
+
 	return makeProjectFiles();
 })();
+
+const pure = (name) =>
+`import React, { Component } from 'react';
+
+export const ${name} = (props) => (
+	<div></div>
+);`;
+
+const es6 = (name) =>
+`import React, { Component } from 'react';
+
+export class ${name} extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+
+	}
+
+	render(){
+		return(
+			<div><div>
+		)
+	}
+};`;
